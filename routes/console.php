96339f8945ce769e3651c10706f63b26ca6bc2f6
Frenki Herlambang Prasetyo <frenkiherlambang@gmail.com>
2023-03-10 08:44:18 +0700

    browsershot

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Spatie\Browsershot\Browsershot;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+Artisan::command('testshot', function () {
+    Browsershot::url('https://www.google.com')->setChromePath("/usr/local/bin")->save('google.png');
 })->purpose('Display an inspiring quote');
